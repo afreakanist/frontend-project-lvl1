b74884589ca8a64f8f10cdcda04910e8d68b012a
afreakanist <afreakanist@gmail.com>
2019-08-21 22:18:04 +0600

    step 4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import readlineSync from 'readline-sync';
-export const userName = () => {
+
+export default () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
-
-
-
-
-
