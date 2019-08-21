bb6ebc0597b9c62ec36fa4fc057e1c43b572cb20
afreakanist <afreakanist@gmail.com>
2019-08-21 20:30:30 +0600

    step3

--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 "use strict";
 
+var _ = require("..");
+
 console.log('Welcome to the Brain Games!');
+(0, _.userName)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUNBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSw2QkFBWjtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgbm9kZVxuaW1wb3J0IHsgdXNlck5hbWUgfSBmcm9tICcuLic7XG5jb25zb2xlLmxvZygnV2VsY29tZSB0byB0aGUgQnJhaW4gR2FtZXMhJyk7XG51c2VyTmFtZSgpO1xuIl19
