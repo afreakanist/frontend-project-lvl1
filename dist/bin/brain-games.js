3a73e65720a95ead20e7c7a355b389acb1f19215
afreakanist <afreakanist@gmail.com>
2019-09-10 19:36:17 +0600

    gitignoring node_modules and dist (probably)

--- dist/bin/brain-games.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _ = require("..");
-
-console.log(_.greeting);
-(0, _.userName)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyIsImdyZWV0aW5nIl0sIm1hcHBpbmdzIjoiQUFBQTs7O0FBQ0E7O0FBRUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZQyxVQUFaO0FBQ0EiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgeyBncmVldGluZywgdXNlck5hbWUgfSBmcm9tICcuLic7XG5cbmNvbnNvbGUubG9nKGdyZWV0aW5nKTtcbnVzZXJOYW1lKCk7XG4iXX0=
